5c8d4802effe8bb64b04609f59d6bd6a5698a797
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-07-14 12:31:47 -0300

    animações adicionadas a pagina details e correção de erros

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -10,12 +10,18 @@ export class PokeListComponent implements OnInit{
 
   private setAllPokemons: any;
   public getAllPokemons: any;
+
+  public apiError: boolean = false;
+
   constructor(private pokeApiService: PokeApiService) {}
   ngOnInit(): void {
       this.pokeApiService.apiListAllPokemons.subscribe(
         res => {
           this.setAllPokemons = res.results
         this.getAllPokemons = this.setAllPokemons;
+  },
+  error =>{
+    this.apiError = true;
   })
   }
 
