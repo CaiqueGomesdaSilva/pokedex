ae1a6eadb1e2b4d4e022f47c767c1f4319121e7b
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-07-14 12:18:59 -0300

    inserindo dados no html details e estilizando a página

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,22 +15,31 @@ export class DetailsComponent implements OnInit{
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   constructor(
     private activatedRouter: ActivatedRoute,
     private pokeApiService: PokeApiService
   ){}
+
   ngOnInit(): void {
-      this.pokemon;
+      this.getPokemon;
   }
 
-  get pokemon(){
+  get getPokemon(){
     const id = this.activatedRouter.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
       }
     )
     
